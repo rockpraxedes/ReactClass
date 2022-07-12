2c33a42dd92881fbdc23b7d18a0afe6497eb24bb
Sérgio Praxedes <rock.praxedes@yahoo.com.br>
2022-07-12 14:35:42 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+import NavBar from "./components/NavBar/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Hello</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
